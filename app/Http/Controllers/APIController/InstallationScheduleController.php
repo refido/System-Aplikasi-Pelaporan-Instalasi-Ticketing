f8a643344de834a877804d4627ad9b2d762e9d3c
Muhammad Daffa' A.R <daffavcd@gmail.com>
2022-01-16 10:33:28 +0700

    installationSch,Instance done. Stable check necessary

--- app/Http/Controllers/APIController/InstallationScheduleController.php
+++ app/Http/Controllers/APIController/InstallationScheduleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\APIController;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\InstallationSchedule;
 
 class InstallationScheduleController extends Controller
 {
@@ -14,7 +15,8 @@ class InstallationScheduleController extends Controller
      */
     public function index()
     {
-        //
+        $data = InstallationSchedule::get()->toJson(JSON_PRETTY_PRINT);
+        return response($data, 200);
     }
 
     /**
@@ -25,7 +27,18 @@ class InstallationScheduleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = new InstallationSchedule;
+        $data->code = $request->code;
+        $data->number_of_technicians = $request->number_of_technicians;
+        $data->instance_id = $request->instance_id;
+        $data->technician_id = $request->technician_id;
+        $data->date_instalation = $request->date_instalation;
+        $data->status = $request->status;
+        $data->save();
+
+        return response()->json([
+            "message" => "installation schedule record created"
+        ], 200);
     }
 
     /**
@@ -36,7 +49,14 @@ class InstallationScheduleController extends Controller
      */
     public function show($id)
     {
-        //
+        if (InstallationSchedule::where('id', $id)->exists()) {
+            $data = InstallationSchedule::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($data, 200);
+        } else {
+            return response()->json([
+                "message" => "installation schedule not found"
+            ], 404);
+        }
     }
 
     /**
@@ -48,7 +68,26 @@ class InstallationScheduleController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        if (InstallationSchedule::where('id', $id)->exists()) {
+            $data = InstallationSchedule::find($id);
+            // dd($request->name);
+
+            $data->code = is_null($request->code) ? $data->code : $request->code;
+            $data->number_of_technicians = is_null($request->number_of_technicians) ? $data->number_of_technicians : $request->number_of_technicians;
+            $data->instance_id = is_null($request->instance_id) ? $data->instance_id : $request->instance_id;
+            $data->technician_id = is_null($request->technician_id) ? $data->technician_id : $request->technician_id;
+            $data->date_instalation = is_null($request->date_instalation) ? $data->date_instalation : $request->date_instalation;
+            $data->status = is_null($request->status) ? $data->status : $request->status;
+            $data->save();
+
+            return response()->json([
+                "message" => "records updated successfully"
+            ], 200);
+        } else {
+            return response()->json([
+                "message" => "installation schedule not found"
+            ], 404);
+        }
     }
 
     /**
@@ -59,6 +98,17 @@ class InstallationScheduleController extends Controller
      */
     public function destroy($id)
     {
-        //
+        if (InstallationSchedule::where('id', $id)->exists()) {
+            $data = InstallationSchedule::find($id);
+            $data->delete();
+
+            return response()->json([
+                "message" => "records deleted"
+            ], 202);
+        } else {
+            return response()->json([
+                "message" => "installation schedule not found"
+            ], 404);
+        }
     }
 }
--- app/Http/Controllers/APIController/InstanceController.php
+++ app/Http/Controllers/APIController/InstanceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\APIController;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Instance;
 
 class InstanceController extends Controller
 {
@@ -14,7 +15,8 @@ class InstanceController extends Controller
      */
     public function index()
     {
-        //
+        $data = Instance::get()->toJson(JSON_PRETTY_PRINT);
+        return response($data, 200);
     }
 
     /**
@@ -25,7 +27,21 @@ class InstanceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = new Instance;
+        $data->category_id = $request->category_id;
+        $data->name = $request->name;
+        $data->no_tlpn_pic = $request->no_tlpn_pic;
+        $data->address = $request->address;
+        $data->pic_name = $request->pic_name;
+        $data->image = $request->image;
+        // MOVE UPLOADED FILE TO PUBLIC
+        // MOVE UPLOADED FILE TO PUBLIC
+        // MOVE UPLOADED FILE TO PUBLIC
+        $data->save();
+
+        return response()->json([
+            "message" => "instance record created"
+        ], 200);
     }
 
     /**
@@ -36,7 +52,14 @@ class InstanceController extends Controller
      */
     public function show($id)
     {
-        //
+        if (Instance::where('id', $id)->exists()) {
+            $data = Instance::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($data, 200);
+        } else {
+            return response()->json([
+                "message" => "instance not found"
+            ], 404);
+        }
     }
 
     /**
@@ -48,7 +71,31 @@ class InstanceController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        if (Instance::where('id', $id)->exists()) {
+            $data = Instance::find($id);
+            // dd($request->name);
+
+            $data->category_id = is_null($request->category_id) ? $data->category_id : $request->category_id;
+            $data->name = is_null($request->name) ? $data->name : $request->name;
+            $data->no_tlpn_pic = is_null($request->no_tlpn_pic) ? $data->no_tlpn_pic : $request->no_tlpn_pic;
+            $data->address = is_null($request->address) ? $data->address : $request->address;
+            $data->pic_name = is_null($request->pic_name) ? $data->pic_name : $request->pic_name;
+            $data->image = is_null($request->image) ? $data->image : $request->image;
+
+            // MOVE UPLOADED FILE TO PUBLIC
+            // MOVE UPLOADED FILE TO PUBLIC
+            // MOVE UPLOADED FILE TO PUBLIC
+
+            $data->save();
+
+            return response()->json([
+                "message" => "records updated successfully"
+            ], 200);
+        } else {
+            return response()->json([
+                "message" => "instance not found"
+            ], 404);
+        }
     }
 
     /**
@@ -59,6 +106,20 @@ class InstanceController extends Controller
      */
     public function destroy($id)
     {
-        //
+        if (Instance::where('id', $id)->exists()) {
+            $data = Instance::find($id);
+            $data->delete();
+
+            return response()->json([
+                "message" => "records deleted"
+            ], 202);
+        } else {
+            return response()->json([
+                "message" => "instance not found"
+            ], 404);
+        }
+        // Delete UPLOADED FILE on PUBLIC
+        // Delete UPLOADED FILE on PUBLIC
+        // Delete UPLOADED FILE on PUBLIC
     }
 }
