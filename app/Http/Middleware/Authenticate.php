181085c6eff77ce4d358c881abd60e709ce691b9
Muhammad Daffa' A.R <daffavcd@gmail.com>
2022-02-02 18:33:36 +0700

    change unauthenticated middleware message

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,12 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
+        if (!$request->expectsJson()) {
             return route('login');
         }
+    }
+    protected function unauthenticated($request, array $guards)
+    {
+        abort(response()->json(['error' => 'Unauthenticated.'], 401));
     }
 }
