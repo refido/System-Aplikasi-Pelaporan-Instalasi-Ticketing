43ba661e437b287fff4457c4c0dacacdda052816
Muhammad Daffa' A.R <daffavcd@gmail.com>
2022-03-15 14:50:32 +0700

    fix2

--- app/Http/Controllers/APIController/InstallationController.php
+++ app/Http/Controllers/APIController/InstallationController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\InstallationSchedule;
 
-class Installation extends Controller
+class InstallationController extends Controller
 {
     /**
      * Display a listing of the resource.
